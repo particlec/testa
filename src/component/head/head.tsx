7a67a760e80f59f0cdcf48dc0db19e29b8ec2016
DESKTOP-EIHFT39\particle <906266319@qq.com>
2021-10-08 18:00:39 +0800

    feat: 接口连接测试

--- src/component/head/head.tsx
+++ src/component/head/head.tsx
@@ -1,13 +1,26 @@
 import HeadStyleDiv from "./styled";
+import axios from 'axios';
 
 const Head = () => {
-  return (
-    <HeadStyleDiv>
-      <div className={"head-content"}>
-        <div className={"head-content-title"}>云中君</div>
-        <div className={"head-content-notes"}>反者道之动,弱者道之用</div>
-      </div>
-    </HeadStyleDiv>
-  );
+
+
+    return (
+        <HeadStyleDiv>
+            <div className={"head-content"}>
+                <div className={"head-content-title"}>云中君</div>
+                <div className={"head-content-notes"}>反者道之动,弱者道之用</div>
+                <div onClick={() => {
+                    axios.get("http://wthrcdn.etouch.cn/weather_mini", {
+                        params: {city: "珠海"},
+                    }).then((res: any) => {
+                            console.log(res)
+                        }
+                    )
+
+                }}>test
+                </div>
+            </div>
+        </HeadStyleDiv>
+    );
 };
 export default Head;
